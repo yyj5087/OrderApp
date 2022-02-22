99d61d1cada2be87750811b17a89ac5a9ea871e9
YYJ <naki5087@naver.com>
2022-02-22 11:01:45 +0900

    getView 함수 기본 구조

--- app/src/main/java/com/nepplus/orderapp/Adapters/StoreAdapter.kt
+++ app/src/main/java/com/nepplus/orderapp/Adapters/StoreAdapter.kt
@@ -1,13 +1,27 @@
 package com.nepplus.orderapp.Adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.FragmentManager
+import com.nepplus.orderapp.R
 import com.nepplus.orderapp.datas.StoreData
+import java.util.zip.Inflater
 
 class StoreAdapter(
     val mContext: Context,
     val resId: Int,
     val mList: ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext,resId,mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView // 머가 들어있냐요?
+        if(tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item,null)
+        }
+        val row = tempRow!!
+        return row
+    }
 }
