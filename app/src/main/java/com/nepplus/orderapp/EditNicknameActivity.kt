2efdacf14a289849f1ccae019486b26438f300a6
YYJ <naki5087@naver.com>
2022-02-22 15:59:06 +0900

    프래그먼트로 돌아왔을때, 첨부한 닉네임 반영

--- app/src/main/java/com/nepplus/orderapp/EditNicknameActivity.kt
+++ app/src/main/java/com/nepplus/orderapp/EditNicknameActivity.kt
@@ -25,5 +25,6 @@ class EditNicknameActivity : AppCompatActivity() {
     }
     fun setupValues(){
 
+
     }
 }
--- app/src/main/java/com/nepplus/orderapp/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/nepplus/orderapp/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.nepplus.orderapp.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -36,6 +37,18 @@ class MyProfileFragment : Fragment() {
     }
     fun setValues(){
 
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQ_CODE_NICKNAME){
+            if(resultCode == Activity.RESULT_OK){
+                val newNickname = data!!.getStringExtra("nick")
+                txtNickname.text = newNickname
+            }
+        }
     }
     
 }
