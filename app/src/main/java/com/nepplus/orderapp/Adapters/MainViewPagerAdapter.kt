0ec232083899cb2ea1a3d53d1b02a45ae2725bdd
YYJ <naki5087@naver.com>
2022-02-22 10:11:57 +0900

    페이지별 제목 설정

--- app/src/main/java/com/nepplus/orderapp/Adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/nepplus/orderapp/Adapters/MainViewPagerAdapter.kt
@@ -17,4 +17,12 @@ class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         }
     }
 
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        return when(position){
+            0 -> "피자 주문"
+            else -> "내 정보 설정"
+        }
+    }
+
 }
