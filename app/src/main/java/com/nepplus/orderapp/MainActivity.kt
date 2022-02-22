4cc59aebfa7d3c8888707f4d569c0913e8a09751
YYJ <naki5087@naver.com>
2022-02-22 10:04:55 +0900

    ViewPager / ViewPager 연결 작업 실행 결과로도 화면 이동가능

--- app/src/main/java/com/nepplus/orderapp/MainActivity.kt
+++ app/src/main/java/com/nepplus/orderapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.nepplus.orderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.nepplus.orderapp.Adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+
+    }
+    fun setValues(){
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mAdapter
     }
 }
