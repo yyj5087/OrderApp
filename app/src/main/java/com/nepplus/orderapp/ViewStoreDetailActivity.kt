5471f5059053a46c7b5d62c266a7272b2d91c7df
YYJ <naki5087@naver.com>
2022-02-22 14:46:48 +0900

    첨부해준 데이터 받아오기  - 멤버변수로 만들어서 활용

--- app/src/main/java/com/nepplus/orderapp/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/nepplus/orderapp/ViewStoreDetailActivity.kt
@@ -2,10 +2,23 @@ package com.nepplus.orderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.orderapp.datas.StoreData
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+    lateinit var mStoreData: StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+        setupEvents()
+        setupValues()
+    }
+    fun setupEvents(){
+
+    }
+    fun setupValues(){
+
     }
 }
