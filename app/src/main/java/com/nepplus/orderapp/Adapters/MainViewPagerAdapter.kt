ebbe80461f568a904a0f050040bd8d9d5e1797cc
YYJ <naki5087@naver.com>
2022-02-22 09:55:02 +0900

    ViewPager Adapter 클래스 제작중 -상속 부분 해결

---
+++ app/src/main/java/com/nepplus/orderapp/Adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,8 @@
+package com.nepplus.orderapp.Adapters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+}
