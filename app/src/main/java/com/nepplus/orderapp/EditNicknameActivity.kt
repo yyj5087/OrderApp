46e89373de92d32b86f76ba74ae49d3badbd762b
YYJ <naki5087@naver.com>
2022-02-22 15:45:35 +0900

    닉네임 변경 화면 추가만

---
+++ app/src/main/java/com/nepplus/orderapp/EditNicknameActivity.kt
@@ -0,0 +1,20 @@
+package com.nepplus.orderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setupValues()
+    }
+
+    fun setupEvents(){
+
+    }
+    fun setupValues(){
+
+    }
+}
