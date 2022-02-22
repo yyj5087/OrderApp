43678eef53a6b2d47b40a9af28d5c1e394d89d9c
YYJ <naki5087@naver.com>
2022-02-22 10:14:04 +0900

    도전 과제 - 치킨 가게목록 추가 시에는, offScreenLimit = 3 설정 해줘야 정상 작동.

--- app/src/main/java/com/nepplus/orderapp/MainActivity.kt
+++ app/src/main/java/com/nepplus/orderapp/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     fun setValues(){
         mAdapter = MainViewPagerAdapter(supportFragmentManager)
         mainViewPager.adapter = mAdapter
-
+//     도전 과제 - 치킨 가게목록 추가 시에는, offScreenLimit = 3 설정 해줘야 정상 작동.
         mainTabLayout.setupWithViewPager(mainViewPager)
     }
 }
