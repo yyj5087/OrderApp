019d9a1d2ac03706d70d100f21d878593a619584
YYJ <naki5087@naver.com>
2022-02-22 09:58:51 +0900

    getCount / getItem 오버라이딩 + 내용 구현

--- app/src/main/java/com/nepplus/orderapp/Adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/nepplus/orderapp/Adapters/MainViewPagerAdapter.kt
@@ -1,8 +1,20 @@
 package com.nepplus.orderapp.Adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.nepplus.orderapp.fragments.MyProfileFragment
+import com.nepplus.orderapp.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount() = 2
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position){
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
+    }
 
 }
