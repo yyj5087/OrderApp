13f49ee186708d9be0cc54d306369ee2e894487a
YYJ <naki5087@naver.com>
2022-02-22 10:51:09 +0900

    가게 데이터 표현용 클래스 추가

---
+++ app/src/main/java/com/nepplus/orderapp/datas/StoreData.kt
@@ -0,0 +1,9 @@
+package com.nepplus.orderapp.datas
+
+class StoreData(
+    val name: String,
+    val rating: Double,
+    val phone: String,  //int로하면 첫번째 0이 날라감
+    val logoImageURL: String
+) {
+}
